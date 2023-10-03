e5e11a3a6fb5505cc76328d1d398dfa87a4aaca0
PudimAtomico <101677584+PudimAtomico@users.noreply.github.com>
2023-10-03 10:44:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom(){
+   document.querySelector('.tecla_pom');
+}
 document.querySelector('.#som_tecla_pom').play();
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
